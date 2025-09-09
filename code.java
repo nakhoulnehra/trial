1edf14bd2befa7844ae88bbdc52a68c3f52ab63e
nakhoulnehra <nehranakhoul@gmail.com>
2025-09-09 19:59:23 +0300

    code-java
    created a java code

---
+++ code.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+
+public class code { 
+
+
+    public static void main(String[] args) { 
+
+        Scanner scan = new Scanner(System.in); 
+
+        int x = scan.nextInt(); 
+
+        System.out.printf("the value of x is %d", x);
+
+
+
+        scan.close(); 
+    }
+
+
+}
